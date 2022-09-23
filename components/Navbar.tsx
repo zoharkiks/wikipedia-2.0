79988d6d928bd6986f6471f1f566dddcf669190f
Zohar <zoharkiks@gmail.com>
2022-09-23 22:42:10 +0530

    Add Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
 import { images } from "../constants";
+import { Icon } from "@iconify/react";
 
 const Navbar = () => {
   return (
-    <div className=''>I'm a Navbar
-   </div>
-  )
-}
+    <div className="flex items-center justify-between px-4 py-4 font-linux  md:mx-auto max-w-6xl md:px-6 text-lg">
+      
+      <ul className=" hidden md:flex space-x-10">
+        <li>Tools</li>
+        <li>Contribution</li>
+        <li>Print/Export</li>
+      </ul>
+      <div className="flex items-center">
+        <Icon className="h-8 w-8 md:hidden " icon="bx:menu-alt-left" />
+        <img className="h-7 md:hidden" src={images.logoMob.src} alt="logo-mob" />
+        <img className="h-20 hidden md:inline-flex " src={images.logoDesk.src} alt="logo-mob" />
+      </div>
 
-export default Navbar
+      <ul className="flex space-x-10">
+        <li className="hidden md:inline-flex">Language</li>
+        <li className="hidden md:inline-flex">Talk</li>
+        <li>Login</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
